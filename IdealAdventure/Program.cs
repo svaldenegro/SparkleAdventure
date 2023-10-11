40c86ccc3e42a7bba0727a031e3cd80cf8663595
sebastian <s.vald>
2023-10-11 14:09:19 -0300

    fixed missing .csproj

---
+++ IdealAdventure/Program.cs
@@ -0,0 +1,12 @@
+﻿using IdealAdventure;
+using IdealAdventure.Scenes;
+using Raylib_cs;
+using Sparkle;
+
+GameSettings settings = new GameSettings() {
+    title = "Test - [Sparkle]",
+    windowFlags = ConfigFlags.FLAG_MSAA_4X_HINT | ConfigFlags.FLAG_WINDOW_RESIZABLE
+};
+
+AdventureGame game = new (settings);
+game.Run(new Main("Main Scene"));
